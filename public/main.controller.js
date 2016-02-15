f571e94f0e5ea13e8a23d81a34348aea8d500e73
Gustaf Erickson <gerickson808@gmail.com>
2016-02-15 15:55:17 -0500

    Broken final product

--- public/main.controller.js
+++ public/main.controller.js
@@ -4,20 +4,24 @@ app.config(function($stateProvider) {
 	$stateProvider.state('main', {
 		url: '/',
 		templateUrl: '/main.html',
-		controller: 'MainController'
+		controller: 'MainController',
 			// RESOLVE!
-	})
-})
+		resolve: {
+			users: function(User){
+				return User.findAll();
+			},
+			posts: function(Post){
+				return Post.findAll();
+			}
+		}
+	});
+});
 
-app.controller('MainController', function($scope) {
+app.controller('MainController', function($scope, posts) {
 
- 	/*
-		TODOS: 
-		1 - use js-data to retrieve all users and all posts
-		(HINT: if you want to be fancy, add a resolve block to this state 
-		and retrieve the data there)
+	$scope.allPosts = posts;
+	console.log('posts: ', posts);
 
- 	*/
-})
+});
 
 
